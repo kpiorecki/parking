b621e23400d46ccb6c865c0dd99d5e1f42c89fc4
kpiorecki <kpiorecki@users.noreply.github.com>
2015-05-03 16:51:12 +0200

    Improved maven projects configuration

--- parking-web/src/main/java/com/kpiorecki/parking/web/servlet/SampleServlet.java
+++ parking-web/src/main/java/com/kpiorecki/parking/web/servlet/SampleServlet.java
@@ -2,20 +2,19 @@ package com.kpiorecki.parking.web.servlet;
 
 import java.io.IOException;
 
-import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(value = "/sample", name = "hello-sample")
-public class SampleServlet extends GenericServlet {
+public class SampleServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-		res.getWriter().println("Hello Sample!");
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().println("Hello Sample!");
 	}
-
 }
