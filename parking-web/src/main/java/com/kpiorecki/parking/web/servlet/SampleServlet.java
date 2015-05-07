8b56d71cc5f2fd98b1044675f3122f831ccdc2b7
kpiorecki <kpiorecki@users.noreply.github.com>
2015-05-07 09:16:33 +0200

    Added beans.xml to parking-web

--- parking-web/src/main/java/com/kpiorecki/parking/web/servlet/SampleServlet.java
+++ parking-web/src/main/java/com/kpiorecki/parking/web/servlet/SampleServlet.java
@@ -2,19 +2,42 @@ package com.kpiorecki.parking.web.servlet;
 
 import java.io.IOException;
 
+import javax.inject.Inject;
+import javax.mail.Session;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.slf4j.Logger;
+
+import com.kpiorecki.parking.ejb.util.DateFormatter;
+
 @WebServlet(value = "/sample", name = "hello-sample")
 public class SampleServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
+	@Inject
+	private Logger logger;
+
+	@Inject
+	@DateFormatter
+	private DateTimeFormatter dateFormatter;
+
+	@Inject
+	private Session mailSession;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().println("Hello Sample!");
+		String dateString = new DateTime().toString(dateFormatter);
+		String logMessage = "Sample invocation at " + dateString + " mailSession=" + mailSession.toString();
+
+		logger.info(logMessage);
+
+		resp.getWriter().println(logMessage);
 	}
 }
